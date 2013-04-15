48f0b56e89d80c2a9c6a8d99b0dd3d8e4b4b19de
Nikhil Benesch <me@designbynikhil.com>
2013-04-15 14:26:07 -0400

    Prepare for heroku

--- store/settings.py
+++ store/settings.py
@@ -1,4 +1,5 @@
 # Django settings for store project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,16 +10,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_db',                      # Or path to database file if using sqlite3.
-        'USER': 'djangouser',                      # Not used with sqlite3.
-        'PASSWORD': 'aran1025',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = {}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -45,7 +37,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/arankhanna/websites/crimson/store/media/'
+root = os.path.dirname(os.path.realpath(__file__))
+MEDIA_ROOT = root + '/../media/' 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,7 +50,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/arankhanna/websites/crimson/store/static/'
+STATIC_ROOT = root + '/../static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -108,7 +101,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/arankhanna/websites/crimson/store/templates"
+    root + "/../templates/"
 )
 
 INSTALLED_APPS = (
@@ -154,3 +147,6 @@ LOGGING = {
         },
     }
 }
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
