bef4484c8ca4fd1b94a97e0c04b27f6f194b89cb
Luis Perez <luisperez@college.harvard.edu>
2013-04-23 22:19:29 -0400

    Tests

--- media/js/custom.js
+++ media/js/custom.js
@@ -1,35 +1,32 @@
-$(document).ready(
-
-  var names = '';
-  var base_url = {{ base_url }} + 'checkout';
-  $('div.item-name').each(function() {
-    var text = this.text();
-    names += "#" + text
-  })
+var names = '';
+var base_url = {{ base_url }} + 'checkout';
+$('div.item-name').each(function() {
+  var text = this.text();
+  names += "#" + text
+})
 
 
-  simpleCart({
+simpleCart({
 
-    cartColumns: [
-      { attr: "name", label: "Name"},
-      { view: "currency", attr: "price", label: "Price"},
-      { view: "decrement", label: false},
-      { attr: "quantity", label: "Qty"},
-      { view: "increment", label: false},
-      { view: "currency", attr: "total", label: "SubTotal" },
-      { view: "remove", text: "Remove", label: false}
-    ],
+  cartColumns: [
+    { attr: "name", label: "Name"},
+    { view: "currency", attr: "price", label: "Price"},
+    { view: "decrement", label: false},
+    { attr: "quantity", label: "Qty"},
+    { view: "increment", label: false},
+    { view: "currency", attr: "total", label: "SubTotal" },
+    { view: "remove", text: "Remove", label: false}
+  ],
 
-      checkout: {
-        type: "SendForm",
-        
-        url: base_url,
+    checkout: {
+      type: "SendForm",
+      
+      url: base_url,
 
-        extra_data: {
-          item_names : data
-          amount: simpleCart.total();
-        }
-
+      extra_data: {
+        item_names : data
+        amount: simpleCart.total();
       }
-    });
-);
+
+    }
+  });
