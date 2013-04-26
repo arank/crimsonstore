8914c02f9e7f880dfc32e3b4c9d1a6f01fd52e72
Luis Perez <luisperez@college.harvard.edu>
2013-04-25 22:15:31 -0400

    Done

--- webstore/views.py
+++ webstore/views.py
@@ -125,7 +125,7 @@ def Success(request):
   if context['verified'] == 'yes':
     return render_to_response("success.html", context, context_instance=RequestContext(request))
   else:
-    return render_to_response('wrong_order.html'), context, context_instance=RequestContext(request))
+    return render_to_response('wrong_order.html', context, context_instance=RequestContext(request))
 
 def Cancel(request):
   return render_to_response('cancel.html', context_instance=RequestContext(request))
