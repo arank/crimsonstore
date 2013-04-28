0c8d306e7f98379370ea0127473f9270b1d8adfc
Luis Perez <luisperez@college.harvard.edu>
2013-04-28 17:01:50 -0400

    Modyfied URL to avoid future errors

--- store/urls.py
+++ store/urls.py
@@ -36,8 +36,9 @@ urlpatterns += patterns('webstore.views',
    url(r'^checkout-cancel/$', 'Cancel', name='cancel'),
 )
 
+# url for PayPal
+urlpatterns += patterns('webstore.views',
+  url(r'^cr!ms0n/p4yp5l/E2E7135958416E4B12258FD3641FD/OwEv0w0ZVt/$', PaypalIPN()))
+
 # static media and file URLs
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
-
-# url for PayPal
-(r'^cr!ms0n/p4yp5l/E2E7135958416E4B12258FD3641FD/OwEv0w0ZVt/$', PaypalIPN())
